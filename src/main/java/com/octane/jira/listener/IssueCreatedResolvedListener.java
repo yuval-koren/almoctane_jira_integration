0d4330d7be951ff4a406bce4c921d0416097c9a9
Yuval Koren <yuval.koren@hp.com>
2016-06-29 14:30:39 +0300

    Added code that suppose to add a defect to octane

--- src/main/java/com/octane/jira/listener/IssueCreatedResolvedListener.java
+++ src/main/java/com/octane/jira/listener/IssueCreatedResolvedListener.java
@@ -12,13 +12,25 @@ import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.issue.IssueInputParameters;
 import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.user.ApplicationUser;
+import com.hpe.adm.nga.sdk.NGA;
+import com.hpe.adm.nga.sdk.Query;
+import com.hpe.adm.nga.sdk.authorisation.Authorisation;
+import com.hpe.adm.nga.sdk.metadata.Metadata;
+import com.hpe.adm.nga.sdk.model.EntityModel;
+import com.hpe.adm.nga.sdk.model.FieldModel;
+import com.hpe.adm.nga.sdk.model.ReferenceFieldModel;
+import com.hpe.adm.nga.sdk.model.StringFieldModel;
+import com.hpe.adm.nga.sdk.utils.CommonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import java.util.*;
+
 public class IssueCreatedResolvedListener implements InitializingBean, DisposableBean {
     private static final Logger log = LoggerFactory.getLogger(IssueCreatedResolvedListener.class);
+    private static final String DEFECT = "defects";
 
     private final EventPublisher eventPublisher;
 
@@ -67,6 +79,53 @@ public class IssueCreatedResolvedListener implements InitializingBean, Disposabl
         } else if (eventTypeId.equals(EventType.ISSUE_CLOSED_ID)) {
             log.info("Issue {} has been closed at {}.", issue.getKey(), issue.getUpdated());
         }
+
+
+        //HttpUtils.SetSystemKeepAlive(false);
+        //HttpUtils.SetSystemProxy();
+
+        final ConfigurationUtils configuration = ConfigurationUtils.getInstance();
+        String url = configuration.getString("sdk.url");
+        Authorisation authorisation = AuthorisationUtils.getAuthorisation();
+        String sharedSpaceId = configuration.getString("sdk.sharedSpaceId");
+        String workspaceId = configuration.getString("sdk.workspaceId");
+
+        NGA nga = ContextUtils.getContextWorkspace(url, authorisation, sharedSpaceId, workspaceId);
+        Metadata metadata = nga.metadata();
+
+        Set<FieldModel> fields = new HashSet<FieldModel>();
+        Query query = new Query().field("subtype").equal("work_item_root").build();
+        Collection<EntityModel> roots = nga.entityList("work_items").get().query(query).execute();
+        EntityModel root = roots.iterator().next();
+
+        FieldModel parentField = new ReferenceFieldModel("parent", root);
+
+        Collection<EntityModel> users = nga.entityList("workspace_users").get().execute();
+        EntityModel user = users.iterator().next();
+        FieldModel author = new ReferenceFieldModel("author", user);
+
+        Query query2 = new Query().field("entity").equal("defect").build();
+        Collection<EntityModel> phases = nga.entityList("phases").get().query(query2).execute();
+        EntityModel phase = phases.iterator().next();
+        FieldModel phaseField = new ReferenceFieldModel("phase", phase);
+
+        FieldModel name = new StringFieldModel("name", "sdk_defect_" + UUID.randomUUID());
+
+        Collection<EntityModel> listNodes = nga.entityList("list_nodes").get().execute();
+        EntityModel severity = CommonUtils.getEntityWithStringValue(listNodes, "logical_name", "list_node.severity.low");
+        FieldModel severityField = new ReferenceFieldModel("severity", severity);
+
+        fields.add(name);
+        fields.add(author);
+        fields.add(phaseField);
+        fields.add(parentField);
+        fields.add(severityField);
+
+        Collection<EntityModel> entities = new ArrayList<EntityModel>();
+        EntityModel defect = new EntityModel(fields);
+        entities.add(defect);
+        nga.entityList(DEFECT).create().entities(entities).execute();
+
     }
 
     /**
